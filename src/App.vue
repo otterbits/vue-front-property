82377b280e25f851dc2404ca665f32f45cdbcbb5
uoohoon <hoonixox@naver.com>
2023-12-14 23:50:43 +0900

    init vue-front-property

---
+++ src/App.vue
@@ -0,0 +1,163 @@
+<template>
+
+<Transition name="fade">
+  <Modal @closeModal="모달창열렸니 = false" :원룸들="원룸들" :누른거="누른거" :모달창열렸니 ="모달창열렸니" />
+</Transition>
+
+  <div class="menu">
+    <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
+  </div>
+
+  <Discount/>
+
+  <button @click="strSort()">가나다정렬</button>
+  <button @click="priceAscend()">가격오름차순정렬</button>
+  <button @click="priceDescend()">가격내름차순정렬</button>
+  <button @click="priceless()">50만원이하</button>
+  <button @click="sortBack()">되돌리기</button>
+
+  
+  <Card @openModal="모달창열렸니 = true; 누른거 = $event" :원룸="원룸들[i]" v-for="(a,i) in 원룸들" :key="a"/>
+
+</template>
+
+<script>
+
+import data from './assets/oneroom.js';
+import Discount from './Discount.vue';
+import Modal from './Modal.vue';
+import Card from './Card.vue';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      원룸들오리지널 : [...data],
+      누른거 : 0,
+      모달창열렸니 : false,
+      신고수 : [0, 0, 0],
+      메뉴들 : ['Home', 'Shops', 'About'],
+      원룸들 : [...data],
+    }
+  },
+  methods: {
+    increase(){
+      this.신고수 += 1;
+    },
+
+    strSort(){
+      this.원룸들.sort(function(a,b){
+        return a.title.localeCompare(b.title);
+      })
+    },
+    priceAscend(){
+      this.원룸들.sort(function(a,b){
+        return a.price - b.price;
+      })
+    },
+
+    priceDescend(){
+      this.원룸들.sort(function(a,b){
+        return b.price - a.price;
+      })
+    },
+
+    priceless(){
+      const filteredRooms = this.원룸들.filter(function(a){
+        return a.price < 500000;
+          return filteredRooms
+      })
+    },
+    sortBack(){
+      this.원룸들 = [...this.원룸들오리지널];
+    },
+  },
+  components: {
+    Discount : Discount,
+    Modal : Modal,
+    Card : Card,
+  }
+}
+</script>
+
+<style>
+.fade-enter-from {
+  transform: translateY(-1000px);
+}
+.fade-enter-active {
+  transition: all 1s;
+}
+.fade-enter-to {
+  transform: translateY(0px);
+}
+
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: all 1s;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+
+body {
+  margin : 0
+}
+div {
+  box-sizing: border-box;
+}
+
+.discount {
+  background: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+}
+
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.close-bt {
+  float: right;
+  font-weight: bold;
+  color: #FFFFFF;
+  background: #3d8ad7;
+  padding: 5px;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+
+}
+</style>
---
+++ src/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="black-bg" v-if="모달창열렸니 == true">
+  <div class="white-bg">
+    <img :src="원룸들[누른거].image" height="200px">
+    <h4>{{ 원룸들[누른거].title }}</h4>
+    <p>{{ 원룸들[누른거].content }}</p>
+    <input v-model="month">
+    <p>{{ month }}개월 선택함 : {{ 원룸들[누른거].price * month }}원</p>  
+  <Discount/>
+  <span class="close-bt" @click="$emit('closeModal')">닫기</span>
+</div>
+  </div>
+</template>
+
+<script>
+import Discount from './Discount.vue';
+
+export default {
+    name : 'Modal',
+    data(){
+      return {
+        month : 1,
+      }
+    },
+    watch : {
+      month(a){
+        if (isNaN(a)) {
+          alert('문자 입력하지 마셈')
+          this.month = 1;
+        }
+      }
+    },
+    components : {
+      Discount : Discount,
+    },
+    props : {
+      원룸들 : Array,
+      누른거 : Number,
+      모달창열렸니 : Boolean,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
